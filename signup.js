c13ed95b20c33de28933b9bca4daa706e97fb353
Shashi kant <39220272+err-shashi272@users.noreply.github.com>
2018-05-15 23:21:13 +0530

    Update signup.js

--- signup.js
+++ signup.js
@@ -70,7 +70,7 @@ var Login = {
 window.fbAsyncInit = function() {
     // FB JavaScript SDK configuration and setup
     FB.init({
-      appId      : '1003110159784040', // FB App ID
+      appId      : '####################', // FB App ID
       cookie     : true,  // enable cookies to allow the server to access the session
       xfbml      : true,  // parse social plugins on this page
       version    : 'v2.8' // use graph api version 2.8
